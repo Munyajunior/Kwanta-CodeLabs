33a95194c2b5023c5b213e81f94ab4c2ff9265c8
munyajunior <ivojunior671@gmail.com>
2025-07-13 04:51:27 +0100

    ✨ feat(fastAPI): add basic FastAPI endpoint and configurations - add a simple hello world endpoint - include uvicorn for local server execution with hot reload - add mypy for static type checking - configure .gitignore to exclude venv

--- python/fastAPI/fastapi_project/main.py
+++ python/fastAPI/fastapi_project/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI(title="KwantaBit CodeLabs FastAPI Tuto",)
 
 @app.get('/')
 async def index():
     return {"message" : "Hello World!"}
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
